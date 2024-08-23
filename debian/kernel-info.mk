857b29f41216a279d6d390134b15cee68ef33509
Marsh <1208879283@qq.com>
2024-08-23 07:55:35 +0000

    disable SELINUX

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.4.78
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.imagetype=sfi build_number=AAZ108 androidboot.build_number=AAZ108 coherent_pool=1280K buildvariant=user console=tty0 droidian.lvm.prefer datapart=/dev/mmcblk0p77 selinux=0 androidboot.selinux=permissive
+KERNEL_BOOTIMAGE_CMDLINE = earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.imagetype=sfi build_number=AAZ108 androidboot.build_number=AAZ108 coherent_pool=1280K buildvariant=user console=tty0 droidian.lvm.prefer datapart=/dev/mmcblk0p77
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
--- debian/linux-headers-4.4.78-blackberry-athena/usr/src/linux-headers-4.4.78-blackberry-athena/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Aug 20 07:28:39 UTC 2024"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "14ef8085ba48"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
